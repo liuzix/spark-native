34b76106579902c73b497b7539b6210a342249d1
liuzix <lzx@uchicago.edu>
2017-05-05 20:22:16 -0500

    Update Test.scala

--- src/main/scala/Test.scala
+++ src/main/scala/Test.scala
@@ -7,11 +7,8 @@ import com.liuzix.SparkNative._
 object Test {
   def main(args: Array[String]) {
     
-    val logFile = "s3n://spark-native-test/README" // Should be some file on your system
     val conf = new SparkConf().setAppName("Simple Application")
     val sc = new SparkContext(conf)
-    sc.hadoopConfiguration.set("fs.s3n.awsAccessKeyId", "AKIAICRKWIRNQ7O3463A")
-    sc.hadoopConfiguration.set("fs.s3n.awsSecretAccessKey", "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")
     var myjob = new TestNativeJob
     myjob.compile("/tmp/TestInt.cpp")
     val intRdd = sc.parallelize(1 to 1000, 10)
